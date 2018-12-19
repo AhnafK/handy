2d8736d28260cc431e68dbd31491aa35b8c9a679
AhnafK <akazi1@stuy.edu>
2018-12-19 10:39:17 -0500

    sshhhh

--- pipe_networking.c
+++ pipe_networking.c
@@ -13,9 +13,13 @@
 int server_handshake(int *to_client) {
   mkfifo("wkp",0644);
   int wkpfd = open("wkp",O_RDONLY);
-  char *wkpbf = malloc(HANDSHAKE_BUFFER_SIZE);
-  printf("");
-  read(wkpfd, wkpbf, HANDSHAKE_BUFFER_SIZE);
+  char ppname[HANDSHAKE_BUFFER_SIZE];
+  printf("Connecting to:\t");
+  read(wkpfd, ppname, HANDSHAKE_BUFFER_SIZE);
+  int pp = open(pp,O_WRONLY);
+  printf("%s",ppname);
+  char sresp[HANDSHAKE_BUFFER_SIZE] = "SERVER";
+  write(pp, sresp, HANDSHAKE_BUFFER_SIZE);
   return 0;
 }
 
@@ -31,7 +35,11 @@ int server_handshake(int *to_client) {
   =========================*/
 int client_handshake(int *to_server) {
   int wkpfd = open("wkp",O_RDONLY);
-  char entr[HANDSHAKE_BUFFER_SIZE];
-  
+  char entr[HANDSHAKE_BUFFER_SIZE] = "pp";
+  mkfifo("pp",0644);
+  write(wkpfd, entr,HANDSHAKE_BUFFER_SIZE);
+  int pp = open("pp", O_RDONLY);
+  char response[HANDSHAKE_BUFFER_SIZE];
+  read(pp, response, HANDSHAKE_BUFFER_SIZE);
   return 0;
 }
