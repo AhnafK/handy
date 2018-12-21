fb2332b500d8d5440101d411fa66a73ec6bea32a
AhnafK <akazi1@stuy.edu>
2018-12-21 10:56:42 -0500

    works

--- pipe_networking.c
+++ pipe_networking.c
@@ -11,15 +11,25 @@
   returns the file descriptor for the upstream pipe.
   =========================*/
 int server_handshake(int *to_client) {
-  mkfifo("wkp",0644);
-  int wkpfd = open("wkp",O_RDONLY);
-  char ppname[HANDSHAKE_BUFFER_SIZE];
-  printf("Connecting to:\t");
-  read(wkpfd, ppname, HANDSHAKE_BUFFER_SIZE);
-  int pp = open(pp,O_WRONLY);
-  printf("%s",ppname);
+  printf("started server\n");
+  int fifO = mkfifo("mario", 0644);
+  printf("created " );
+  int wkp = open("mario", O_RDONLY);
+  printf("and opened well known pipe\n");
+  //if(fifO == -1){
+  //  printf("failure\n");
+  //  return -1;
+  //}
+  char buffer[HANDSHAKE_BUFFER_SIZE];
+  read(wkp, buffer, HANDSHAKE_BUFFER_SIZE);
+  printf("recieved %s\n", buffer);
+  int pp = open(buffer,O_WRONLY);
+  printf("%s",buffer);
   char sresp[HANDSHAKE_BUFFER_SIZE] = "SERVER";
   write(pp, sresp, HANDSHAKE_BUFFER_SIZE);
+  close(wkp);
+  close(pp);
+  printf("done\n");
   return 0;
 }
 
@@ -34,12 +44,14 @@ int server_handshake(int *to_client) {
   returns the file descriptor for the downstream pipe.
   =========================*/
 int client_handshake(int *to_server) {
-  int wkpfd = open("wkp",O_RDONLY);
+  int wkpfd = open("mario",O_WRONLY);
+  printf("Client connected to wkp\n");
   char entr[HANDSHAKE_BUFFER_SIZE] = "pp";
   mkfifo("pp",0644);
   write(wkpfd, entr,HANDSHAKE_BUFFER_SIZE);
   int pp = open("pp", O_RDONLY);
   char response[HANDSHAKE_BUFFER_SIZE];
   read(pp, response, HANDSHAKE_BUFFER_SIZE);
+  printf("%s\n", response);
   return 0;
 }
